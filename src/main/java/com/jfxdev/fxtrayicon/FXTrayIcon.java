6209e6e8ab9076db53e7e9c7b8acf68a129a0baf
dustinkredmond <42328885+dustinkredmond@users.noreply.github.com>
2020-08-22 11:30:41 -0400

    Add more API and add runnable test

--- src/main/java/com/jfxdev/fxtrayicon/FXTrayIcon.java
+++ src/main/java/com/jfxdev/fxtrayicon/FXTrayIcon.java
@@ -4,18 +4,33 @@ import javafx.application.Platform;
 import javafx.stage.Stage;
 
 import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import java.awt.AWTException;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
 import java.io.IOException;
 import java.net.URL;
 
+@SuppressWarnings("unused")
 public class FXTrayIcon {
 
     private final SystemTray tray = SystemTray.getSystemTray();
-    private Stage parentStage;
+    private final Stage parentStage;
     private final TrayIcon trayIcon;
     private final PopupMenu popupMenu = new PopupMenu();
-    private boolean addExitMenuItem = false;
+    /**
+     * Assume this as {@code true} by default. Otherwise
+     * a user would have to implement this MenuItem themselves
+     * and thus we would need to expose AWT objects.
+     */
+    private boolean addExitMenuItem = true;
 
 
     public FXTrayIcon(Stage parentStage, URL iconImagePath) {
@@ -24,25 +39,29 @@ public class FXTrayIcon {
                     "supported by the current desktop environment.");
         }
 
-        // Keep the tray icon after all windows are closed
+        // Keeps the JVM running even if there are no
+        // visible JavaFX Stages
         Platform.setImplicitExit(false);
 
+        // Set the SystemLookAndFeel, if not available, use default
+        // User could change this by calling UIManager.setLookAndFeel themselves
+        // after instantiating the FXTrayIcon
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (ClassNotFoundException | InstantiationException
                 | IllegalAccessException | UnsupportedLookAndFeelException ignored) {}
+
         try {
             final Image iconImage = ImageIO.read(iconImagePath)
+                    // Some OSes do not behave well if the icon is larger than 16x16
+                    // Image.SCALE_SMOOTH will provide the best quality icon in most instances
                     .getScaledInstance(16, 16, Image.SCALE_SMOOTH);
             this.parentStage = parentStage;
-            this.trayIcon = new TrayIcon(iconImage, "", popupMenu);
+            this.trayIcon = new TrayIcon(iconImage, parentStage.getTitle(), popupMenu);
         } catch (IOException e) {
-            // will probably get here if the resource is not found or otherwise
-            // unable to be read
-            throw new RuntimeException(e);
+            throw new RuntimeException("Unable to read the Image at the provided path.");
         }
     }
-
 
     /**
      * Adds the FXTrayIcon to the system tray.
@@ -54,12 +73,18 @@ public class FXTrayIcon {
         SwingUtilities.invokeLater(() -> {
             try {
                 tray.add(this.trayIcon);
-                this.popupMenu.addSeparator();
+
+                // Add a MenuItem with the main Stage's title, this will show the
+                // main JavaFX stage when clicked.
                 String miTitle = parentStage.getTitle().isEmpty() ? "Show application" : parentStage.getTitle();
                 MenuItem miStage = new MenuItem(miTitle);
                 miStage.setFont(Font.decode(null).deriveFont(Font.BOLD));
-                miStage.addActionListener(e -> Platform.runLater(() -> parentStage.show()));
+                miStage.addActionListener(e -> Platform.runLater(parentStage::show));
                 this.popupMenu.add(miStage);
+
+                // If Platform.setImplicitExit(false) then the JVM will continue to run after
+                // no more Stages remain, thus we provide a way to terminate it by default.
+                // User will be able to override this by calling new FXTrayIcon(...).addExitItem(false)
                 if (addExitMenuItem) {
                     MenuItem miExit = new MenuItem("Exit program");
                     miExit.addActionListener(e -> {
@@ -68,8 +93,10 @@ public class FXTrayIcon {
                     });
                     this.popupMenu.add(miExit);
                 }
+                // Add a separator between user-defined MenuItems and our built-in ones
+                this.popupMenu.addSeparator();
             } catch (AWTException e) {
-                throw new RuntimeException(e);
+                throw new RuntimeException("Unable to add TrayIcon", e);
             }
         });
     }
@@ -86,10 +113,82 @@ public class FXTrayIcon {
     }
 
     /**
+     * Removes the MenuItem at the given index
+     * @param index Index of the MenuItem to remove
+     */
+    public void removeMenuItem(int index) {
+        EventQueue.invokeLater(() -> {
+            this.popupMenu.remove(index);
+        });
+    }
+
+    /**
+     * Removes the specified item from the Menu
+     * @param menuItem Item to be removed, this method does
+     *                 nothing if the item is not in the
+     *                 Menu.
+     */
+    public void removeMenuItem(MenuItem menuItem) {
+        EventQueue.invokeLater(() -> {
+            this.popupMenu.remove(menuItem);
+        });
+    }
+
+    /**
+     * Adds a separator line to the Menu at the current position.
+     */
+    public void addSeparator() {
+        EventQueue.invokeLater(this.popupMenu::addSeparator);
+    }
+
+    /**
+     * Adds a separator line to the Menu at the given position.
+     * @param index The position at which to add the separator
+     */
+    public void insertSeparator(int index) {
+        EventQueue.invokeLater(() -> this.popupMenu.insertSeparator(index));
+    }
+
+    /**
+     * Adds the specified MenuItem to the Menu
+     * @param item Item to be added
+     */
+    public void addMenuItem(MenuItem item) {
+        EventQueue.invokeLater(() -> this.popupMenu.add(item));
+    }
+
+    /**
+     * Returns the MenuItem at the given index.
+     * <p>
+     *     NOTE: This should be called via the
+     *     {@code EventQueue.invokeLater()} method as well as any
+     *     subsequent operations on the MenuItem that is returned.
+     * @param index Index of the MenuItem to be returned.
+     * @return The MenuItem at {@code index}
+     */
+    public MenuItem getMenuItem(int index) {
+        return this.popupMenu.getItem(index);
+    }
+
+    /**
+     * Sets the FXTrayIcon's tooltip that is displayed on mouse hover.
+     * @param tooltip The text of the tooltip
+     */
+    public void setTrayIconTooltip(String tooltip) {
+        EventQueue.invokeLater(() -> this.trayIcon.setToolTip(tooltip));
+    }
+
+    /**
      * Removes the {@code FXTrayIcon} from the system tray.
+     * Also calls {@code Platform.setImplicitExit(true)}, thereby
+     * allowing the JVM to terminate after the last JavaFX {@code Stage}
+     * is hidden.
      */
     public void hide() {
-        EventQueue.invokeLater(() -> tray.remove(trayIcon));
+        EventQueue.invokeLater(() -> {
+            tray.remove(trayIcon);
+            Platform.setImplicitExit(true);
+        });
     }
 
 }
--- src/test/java/com/jfxdev/fxtrayicon/IconTest.java
+++ src/test/java/com/jfxdev/fxtrayicon/IconTest.java
@@ -1,25 +1,63 @@
 package com.jfxdev.fxtrayicon;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.awt.MenuItem;
+
 public class IconTest extends Application {
+
     @Override
     public void start(Stage stage) throws Exception {
-        BorderPane root = new BorderPane();
-        root.setCenter(new Label("This is a test."));
-        stage.setScene(new Scene(root));
-        stage.setTitle("My Application");
-        stage.sizeToScene();
-        stage.show();
 
+        BorderPane root = new BorderPane();
+        stage.setScene(new Scene(root));
+
+        // By default, our FXTrayIcon will have an entry with our Application's title in bold font,
+        // when clicked, this MenuItem will call stage.show()
+        //
+        // This can be disabled by simply removing the MenuItem after instantiating the FXTrayIcon
+        // though, by convention, most applications implement this functionality.
+        stage.setTitle("FXTrayIcon test!");
+
+        // Instantiate the FXTrayIcon providing the parent Stage and a path to an Image file
         FXTrayIcon trayIcon = new FXTrayIcon(stage, getClass().getResource("icons8-link-64.png"));
-        trayIcon.persistIconOnLastJavaFXStageHidden(true);
-        trayIcon.addExitItem(true);
         trayIcon.show();
 
+        // By default the FXTrayIcon's tooltip will be the parent stage that we passed in the constructor
+        // This method can override this
+        trayIcon.setTrayIconTooltip("An alternative tooltip!");
+
+        // Note, we need to use the AWT MenuItem, not JavaFX's MenuItem
+        MenuItem menuItemTest = new MenuItem("Create some JavaFX component!");
+        // If we're going to modify the scene graph of JavaFX, this must be done
+        // on the JavaFX application thread. Thus, we must wrap this change in
+        // a call to Platform.runLater
+        menuItemTest.addActionListener(e -> {
+            Platform.runLater(() -> {
+                new Alert(Alert.AlertType.INFORMATION, "We just called JavaFX from an AWT menu!").showAndWait();
+            });
+        });
+        trayIcon.addMenuItem(menuItemTest);
+
+        VBox vBox = new VBox(5);
+        vBox.getChildren().add(new Label("You should see a tray icon!"));
+        Button buttonRemoveTrayIcon = new Button("Remove TrayIcon");
+        vBox.getChildren().add(buttonRemoveTrayIcon);
+
+        // Removing the FXTrayIcon, this will also cause the JVM to terminate
+        // after the last JavaFX Stage is hidden
+        buttonRemoveTrayIcon.setOnAction(e -> trayIcon.hide());
+
+        root.setCenter(vBox);
+        stage.sizeToScene();
+        stage.show();
     }
 }
