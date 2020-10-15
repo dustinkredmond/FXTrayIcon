b8c91e1d49a62bf19c759ed46fe8fc39f710896a
dredmond <dustinkredmond@gmail.com>
2020-10-15 11:01:54 -0400

    Add validation for not implemented method support

--- src/main/java/com/jfxdev/fxtrayicon/FXTrayIcon.java
+++ src/main/java/com/jfxdev/fxtrayicon/FXTrayIcon.java
@@ -12,6 +12,7 @@ import javax.swing.UnsupportedLookAndFeelException;
 import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
+import java.util.StringJoiner;
 
 public class FXTrayIcon {
 
@@ -234,6 +235,30 @@ public class FXTrayIcon {
      */
     private MenuItem convertFromJavaFX(javafx.scene.control.MenuItem fxItem) {
         MenuItem awtItem = new MenuItem(fxItem.getText());
+
+        StringJoiner sj = new StringJoiner(",");
+        if (fxItem.getGraphic() != null) {
+            sj.add("setGraphic()");
+        }
+        if (fxItem.getAccelerator() != null) {
+            sj.add("setAccelerator()");
+        }
+        if (fxItem.getCssMetaData().size() > 0) {
+            sj.add("getCssMetaData().add()");
+        }
+        if (fxItem.getOnMenuValidation() != null) {
+            sj.add("setOnMenuValidation()");
+        }
+        if (fxItem.getStyle() != null) {
+            sj.add("setStyle()");
+        }
+        String errors = sj.toString();
+        if (!errors.isEmpty()) {
+            throw new RuntimeException(String.format(
+                    "The following methods were called on the " +
+                    "passed JavaFX MenuItem (%s), these methods are not" +
+                    "supported by FXTrayIcon.", errors));
+        }
 
         // Set the onAction event to be performed via ActionListener action
         if (fxItem.getOnAction() != null) {
