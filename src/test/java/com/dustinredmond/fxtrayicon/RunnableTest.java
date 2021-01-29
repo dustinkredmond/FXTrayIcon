dbf592a3247102b93b9bd67c13a3bba05279b63e
dustinkredmond <42328885+dustinkredmond@users.noreply.github.com>
2021-01-29 16:57:05 -0500

    Move call to FXTrayIcon's show() method
    Signed-off-by: Dustin Redmond <dustin@dustinredmond.com>

--- src/test/java/com/dustinredmond/fxtrayicon/RunnableTest.java
+++ src/test/java/com/dustinredmond/fxtrayicon/RunnableTest.java
@@ -34,7 +34,7 @@ public class RunnableTest extends Application {
 
         // Instantiate the FXTrayIcon providing the parent Stage and a path to an Image file
         FXTrayIcon trayIcon = new FXTrayIcon(stage, getClass().getResource("icons8-link-64.png"));
-        trayIcon.show();
+
 
         // By default the FXTrayIcon's tooltip will be the parent stage's title, that we used in the constructor
         // This method can override this
@@ -87,6 +87,7 @@ public class RunnableTest extends Application {
                 new Alert(Alert.AlertType.INFORMATION, "Invoked setOnAction() method").showAndWait());
 
         root.setCenter(vBox);
+        trayIcon.show();
         stage.sizeToScene();
         stage.show();
     }
