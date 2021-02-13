2136af58dab015880368cc100e702e6321237dcd
dustinkredmond <42328885+dustinkredmond@users.noreply.github.com>
2021-02-13 10:19:51 -0500

    Nit-picky clean up :)

--- src/test/java/com/dustinredmond/fxtrayicon/notaskbaricon/Main.java
+++ src/test/java/com/dustinredmond/fxtrayicon/notaskbaricon/Main.java
@@ -19,69 +19,64 @@ import java.net.URL;
 
 public class Main extends Application {
 
-	private static       FXTrayIcon icon;
-
 	/**
-	 *
-	 *
 	 * Launch the Start class FIRST!
-	 *
-	 *
+	 * @param stage The default JavaFX Stage
 	 */
-
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) {
 		URL iconFile = new RunnableTest().getIcon();
-		primaryStage.initStyle(StageStyle.UTILITY);
-		primaryStage.setHeight(0);
-		primaryStage.setWidth(0);
+		stage.initStyle(StageStyle.UTILITY);
+		stage.setHeight(0);
+		stage.setWidth(0);
 		Stage mainStage = new Stage();
-		mainStage.initOwner(primaryStage);
+		mainStage.initOwner(stage);
 		mainStage.initStyle(StageStyle.UNDECORATED);
-		Label     label = new Label("No TaskBar Icon");
-		Label     label2 = new Label("Type a message and click the button");
+
+		Label label = new Label("No TaskBar Icon");
+		Label label2 = new Label("Type a message and click the button");
 		label2.setAlignment(Pos.CENTER_LEFT);
 		TextField tfInput = new TextField();
 		Button button = new Button("Show Alert");
-		button.setOnAction(e-> showMessage(tfInput.getText()));
+		button.setOnAction(e -> showMessage(tfInput.getText()));
+
 		VBox vbox = new VBox();
 		vbox.setPadding(new Insets(10,20,10,20));
 		vbox.setAlignment(Pos.CENTER);
 		vbox.setSpacing(20);
-		vbox.getChildren().addAll(label,label2,tfInput,button);
-		StackPane root  = new StackPane();
+		vbox.getChildren().addAll(label, label2, tfInput, button);
+
+		StackPane root = new StackPane();
 		root.getChildren().add(vbox);
 		mainStage.setScene(new Scene(root, 250, 200));
 		mainStage.initStyle(StageStyle.UTILITY); //This is what makes the icon disappear in Windows.
-		if (FXTrayIcon.isSupported()){
-			icon = new FXTrayIcon(primaryStage, iconFile);
+		if (FXTrayIcon.isSupported()) {
+			icon = new FXTrayIcon(stage, iconFile);
+
 			MenuItem menuShowStage   = new MenuItem("Show Stage");
 			MenuItem menuHideStage   = new MenuItem("Hide Stage");
 			MenuItem menuShowMessage = new MenuItem("Show Message");
 			MenuItem menuExit        = new MenuItem("Exit");
-			menuShowStage.setOnAction(e->{
-				Platform.runLater(()->com.sun.javafx.application.PlatformImpl.setTaskbarApplication(false));
+			menuShowStage.setOnAction(e -> {
+				Platform.runLater(()-> com.sun.javafx.application.PlatformImpl.setTaskbarApplication(false));
 				mainStage.show();
 			});
-			menuHideStage.setOnAction(e->{
-				Platform.runLater(()->com.sun.javafx.application.PlatformImpl.setTaskbarApplication(true));
+			menuHideStage.setOnAction(e -> {
+				Platform.runLater(() -> com.sun.javafx.application.PlatformImpl.setTaskbarApplication(true));
 				mainStage.hide();
 			});
-			menuShowMessage.setOnAction(e->showMessage());
-			menuExit.setOnAction(e-> System.exit(0));
+			menuShowMessage.setOnAction(e -> showMessage());
+			menuExit.setOnAction(e -> System.exit(0));
 			icon.addMenuItem(menuShowStage);
 			icon.addMenuItem(menuHideStage);
 			icon.addMenuItem(menuShowMessage);
 			icon.addMenuItem(menuExit);
 			icon.showMinimal();
-			//mainStage.show();
 		}
 
 	}
 
- 	public static void main(String[] args) {
-		launch(args);
-	}
+	private FXTrayIcon icon;
 
 	private void showMessage() {
 		icon.showInfoMessage("Check It Out!","Look Ma, No Taskbar Icon!");
--- src/test/java/com/dustinredmond/fxtrayicon/notaskbaricon/Start.java
+++ src/test/java/com/dustinredmond/fxtrayicon/notaskbaricon/Start.java
@@ -1,27 +1,24 @@
 package com.dustinredmond.fxtrayicon.notaskbaricon;
 
+import javafx.application.Application;
+
 public class Start {
 
-
 	/**
-	 *
-	 *
 	 * THIS CLASS MUST BE RUN FIRST!
 	 *
-	 *
+	 * Kicks off the com.dustinredmond.fxtrayicon.notaskbaricon.Main runnable test
+	 * @param args Command line arguments
 	 */
-
-
 	public static void main(String[] args) {
 		// This is an awt property which removes the icon
 		// from the Dock on a mac and the TaskBar on Windows
-
 		System.setProperty("apple.awt.UIElement", "true");
 		java.awt.Toolkit.getDefaultToolkit();
 
 		// This is a call to JavaFX application main method.
 		// From now on we are transferring control to FX application.
-
-		Main.main(args);	}
+		Application.launch(Main.class, args);
+	}
 
 }
